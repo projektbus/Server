32da8477624b9d072a170774d12228fcc011ed48
Sebastian <jpalimaka2179@gmail.com>
2019-03-26 16:48:25 +0100

    Creating Spring Boot

---
+++ ProjektBus/src/main/java/Bus/ProjektBus/ProjektBusApplication.java
@@ -0,0 +1,13 @@
+package Bus.ProjektBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektBusApplication.class, args);
+	}
+
+}
