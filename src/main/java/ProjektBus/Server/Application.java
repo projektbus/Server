c0d9a7213239b9be27e0129317527f8474719cb6
Sebastian <jpalimaka2179@gmail.com>
2019-03-26 22:33:38 +0100

    fanaberie Mariusza

--- src/main/java/ProjektBus/Server/Application.java
+++ src/main/java/ProjektBus/Server/Application.java
@@ -1,4 +1,4 @@
-package Projekt.Bus.Server;
+package ProjektBus.Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ProjektBus/Server/ApplicationTests.java
+++ src/test/java/ProjektBus/Server/ApplicationTests.java
@@ -1,4 +1,4 @@
-package Projekt.Bus.Server;
+package ProjektBus.Server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
