fc3e9c9bbded780e13c3aa77fc27d00c53eb6d36
Mariusz Orłowicz <qmariusz16@gmail.com>
2019-04-04 18:53:46 +0200

    SERVER-4 | Create Rest Endpoints and methods for register

---
+++ src/main/java/ProjektBus/Server/service/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package ProjektBus.Server.service;
+
+import ProjektBus.Server.model.User;
+import ProjektBus.Server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User registerUser(User user) {
+        //Tutaj zapisywanie do bazy
+        user = userRepository.save(user);
+        return user;
+    }
+}
---
+++ src/test/java/ProjektBus/Server/repository/UserServiceTest.java
@@ -0,0 +1,26 @@
+package ProjektBus.Server.repository;
+
+import ProjektBus.Server.model.User;
+import ProjektBus.Server.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void shouldSaveUser() {
+        User user = new User("UserTest", "email@email.com", "haslo");
+        userService.registerUser(user);
+        assertNotNull(user.getId());
+    }
+}
