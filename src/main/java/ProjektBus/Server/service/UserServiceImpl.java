ed5e60b1870d7f85b5e1c4b5b52d6ebaf946c41b
Mariusz Orłowicz <qmariusz16@gmail.com>
2019-04-09 06:11:53 +0000

    Merge branches 'SERVER-4_Create_Rest_endpoints_and_methods_for_register' and 'master' of https://gitlab.com/projektbus/serwer into SERVER-4_Create_Rest_endpoints_and_methods_for_register

---
+++ src/main/java/ProjektBus/Server/service/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package ProjektBus.Server.service;
+
+import ProjektBus.Server.model.User;
+import ProjektBus.Server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User registerUser(User user) {
+        user = userRepository.save(user);
+        return user;
+    }
+}
