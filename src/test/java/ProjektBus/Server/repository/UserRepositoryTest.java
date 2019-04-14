90523270a0214cf50c1b6bb244fb483656804bdc
Mariusz Orłowicz <qmariusz16@gmail.com>
2019-04-14 16:57:40 +0200

    SERVER-12 | Research about mongoDB Atlas

--- src/test/java/ProjektBus/Server/repository/UserRepositoryTest.java
+++ src/test/java/ProjektBus/Server/repository/UserRepositoryTest.java
@@ -1,37 +1,27 @@
-//package ProjektBus.Server.repository;
-//
-//import ProjektBus.Server.model.User;
-//import ProjektBus.Server.service.UserService;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.data.mongodb.core.MongoTemplate;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.util.List;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class UserRepositoryTest {
-//
-//    @Autowired
-//    private UserService userService;
-//
-//    @Autowired
-//    private MongoTemplate mongoTemplate;
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Test
-//    public void create_should_create_new_student() {
-//        User user = new User("a", "b", "c");
-//
-//        userService.registerUser(user);
-//        List<User> studs = userRepository.findAll();
-//        assertEquals("a", studs.get(0).getLogin());
-//    }
-//}
+package ProjektBus.Server.repository;
+
+import ProjektBus.Server.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository repository;
+
+    //TODO add embedded MongoDB
+    @Test
+    public void shouldSaveUser() {
+        repository.deleteAll();
+        User user = repository.save(new User("User1", "email@email.com", "haslo"));
+        assertNotNull(user);
+    }
+
+}
