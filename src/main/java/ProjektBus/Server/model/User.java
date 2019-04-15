4f0a1ad1b9f22fe3d381e0fa1034f01c94edff3e
Marek Smrek <mareksmrekrg@gmail.com>
2019-04-15 12:50:27 +0000

    Validation

--- src/main/java/ProjektBus/Server/model/User.java
+++ src/main/java/ProjektBus/Server/model/User.java
@@ -1,8 +1,13 @@
 package ProjektBus.Server.model;
 
+import jdk.nashorn.internal.objects.annotations.Constructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
@@ -13,11 +18,39 @@ public class User {
     private String login;
     private String email;
     private String password;
+    @Valid
+    public User( @Size(min=5,max=32) @NotNull String login,
+                 @NotNull String email,
+                 @Size(min=5,max=32) @NotNull String password) {
 
-    public User(String login, String email, String password) {
         this.login = login;
         this.email = email;
         this.password = password;
     }
+    //Rzucalo wyjatkiem jak tylko probowalem cokolwiek dodac, nawet bez edycji kodu,
+    // ten pusty konstruktor sprawil ze wszystko u mnie dziala
+    public User() {
 
+    }
+
+    // SetGet? Dodalem gety gdyz dostalem error 404 w mozgu jak mam sie do tego dorwac poza userem
+
+    // Jesli ma byc inaczej to mnie powiedzcie jak dostac sie do tych danych, albo czy tutaj validacje przeniesc cala
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User toUser()
+    {
+        return new User(getLogin(),getEmail(),getPassword());
+    }
 }
--- src/main/java/ProjektBus/Server/service/UserService.java
+++ src/main/java/ProjektBus/Server/service/UserService.java
@@ -1,8 +1,9 @@
 package ProjektBus.Server.service;
-
 import ProjektBus.Server.model.User;
 
 public interface UserService {
 
     User registerUser(User user);
+
+    Integer userValidate( User user);
 }
