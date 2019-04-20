a58c3fdc33ffb35211bfa94e719551af83029c79
Mariusz Orłowicz <qmariusz16@gmail.com>
2019-04-20 09:50:18 +0200

    SERVER-12 | Research about mongoDB Atlas

--- src/main/java/ProjektBus/Server/config/AppConfig.java
+++ src/main/java/ProjektBus/Server/config/AppConfig.java
@@ -40,12 +40,4 @@ public class AppConfig {
         return mailSender;
     }
 
-//    @Bean
-//    public MongoTemplate mongoTemplate() throws IOException {
-//        EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
-//        mongo.setBindIp("localhost");
-//        MongoClient mongoClient = mongo.getObject();
-//        MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, "a");
-//        return mongoTemplate;
-//    }
 }
