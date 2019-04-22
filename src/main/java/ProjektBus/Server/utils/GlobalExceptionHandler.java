4b7f346d81bf690c1f826713a9714b01ef56c6fa
Mariusz Orłowicz <qmariusz16@gmail.com>
2019-04-22 22:02:16 +0200

    SERVER-17 | Exception handling

---
+++ src/main/java/ProjektBus/Server/utils/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package ProjektBus.Server.utils;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    protected ResponseEntity handleIllegalArgumentException(Exception ex, WebRequest request){
+
+        return handleExceptionInternal(ex, "aa", new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
