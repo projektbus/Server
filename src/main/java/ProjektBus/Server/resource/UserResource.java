d2b1f2e82f229be93135131e002249a3985f441b
VarsonChara <mareksmrekrg@gmail.com>
2019-04-24 00:30:24 +0200

    SERVER-10_Userdata_validation

--- src/main/java/ProjektBus/Server/resource/UserResource.java
+++ src/main/java/ProjektBus/Server/resource/UserResource.java
@@ -2,6 +2,7 @@ package ProjektBus.Server.resource;
 
 import ProjektBus.Server.model.ConfirmationToken;
 import ProjektBus.Server.model.User;
+import ProjektBus.Server.model.UserCreateRequestValidator;
 import ProjektBus.Server.service.ConfirmationTokenService;
 import ProjektBus.Server.service.EmailSenderService;
 import ProjektBus.Server.service.UserService;
@@ -10,8 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -27,8 +30,11 @@ public class UserResource {
     @Autowired
     private ConfirmationTokenService confirmationTokenService;
 
+    @Autowired
+    private UserCreateRequestValidator userCreateRequestValidator;
+
     @PostMapping("/user")
-    public ResponseEntity saveUser(@RequestBody User user) throws URISyntaxException {
+    public ResponseEntity saveUser(@RequestBody @Valid User user) throws URISyntaxException {
 
         String passwordEncode = ProjektUtils.passwordEncode(user.getPassword());
         user.setPassword(passwordEncode);
@@ -39,6 +45,11 @@ public class UserResource {
         sendEmailWithConfirmationTokenToUser(user, confirmationToken);
 
         return ResponseEntity.created(new URI("https://peaceful-sierra-14544.herokuapp.com/user?login=" + user.getLogin())).build();
+    }
+
+    @InitBinder("userCreateRequest")
+    public void setupBinder(WebDataBinder binder) {
+        binder.addValidators(userCreateRequestValidator);
     }
 
     @PostMapping("/confirm-account")
