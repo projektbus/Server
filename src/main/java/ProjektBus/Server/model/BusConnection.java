683d7134ddd18eb9f1e34c5d67f7d7cff5f119ff
Michał Nowak <nowaczek5552@gmail.com>
2019-05-25 07:51:28 +0000

    SERVER-23 | Create rest endpoints for bus connection and validator

--- src/main/java/ProjektBus/Server/model/BusConnection.java
+++ src/main/java/ProjektBus/Server/model/BusConnection.java
@@ -1,8 +1,12 @@
 package ProjektBus.Server.model;
 
+import ProjektBus.Server.validation.validationInterfaces.ExistBusLine;
+import ProjektBus.Server.validation.validationInterfaces.ExistBusStop;
+import ProjektBus.Server.validation.validationInterfaces.ExistCarrier;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalTime;
 
 @Data
@@ -14,10 +18,16 @@ public class BusConnection {
 
     @Id
     private String id;
-
+    @NotNull(message = "{field.required}")
+    @ExistBusLine(message = "{busLine.exists}")
     private String busLineId;
+    @NotNull(message = "{field.required}")
+    @ExistCarrier(message = "{carrier.exists}")
     private String carrierId;
+    @NotNull(message = "{field.required}")
+    @ExistBusStop(message = "{busStop.exists}")
     private String busStopId;
+    @NotNull(message = "{field.required}")
     private LocalTime departureTime;
 
     public BusConnection(String busLineId, String carrierId, String busStopId, LocalTime departureTime) {
---
+++ src/main/java/ProjektBus/Server/resource/BusConnectionResource.java
@@ -0,0 +1,81 @@
+package ProjektBus.Server.resource;
+
+import ProjektBus.Server.model.BusConnection;
+import ProjektBus.Server.service.BusConnectionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
+public class BusConnectionResource {
+    @Autowired
+    private BusConnectionService busConnectionService;
+
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
+    @PostMapping("/bus-connections")
+    public ResponseEntity saveBusConnection(@Valid @RequestBody BusConnection busConnection) throws URISyntaxException {
+        busConnectionService.addBusConnection(busConnection);
+        return ResponseEntity.created(new URI("https://peaceful-sierra-14544.herokuapp.com/bus-connections/" + busConnection.getId())).build();
+    }
+
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
+    @GetMapping("/bus-connections")
+    public @ResponseBody
+    ResponseEntity getBusConnections() {
+        return new ResponseEntity(busConnectionService.getAllBusConnections(), HttpStatus.OK);
+
+    }
+
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
+    @DeleteMapping("/bus-connections/{id}")
+    public ResponseEntity deleteBusConnection(@PathVariable("id") String id) {
+        BusConnection busConnection = busConnectionService.getBusConnectionById(id);
+        if (busConnection != null) {
+            busConnectionService.deleteBusConnection(busConnection);
+            return new ResponseEntity("Bus connection deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity("Bus connection does not exist", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
+    @GetMapping("/bus-connections/{id}")
+    public @ResponseBody
+    ResponseEntity getBusConnection(@PathVariable("id") String id) {
+        if (null != busConnectionService.getBusConnectionById(id)) {
+            return new ResponseEntity(busConnectionService.getBusConnectionById(id), HttpStatus.OK);
+        } else {
+            return new ResponseEntity("Bus connection does not exist", HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
+    @GetMapping("/bus-connections/bus-lines/{busLineId}/bus-stops/{busStopId}")
+    public @ResponseBody
+    ResponseEntity getBusConnectionByBusLineAndBusStop(@PathVariable("busLineId") String busLineId, @PathVariable("busStopId") String busStopId) {
+        if (null != busConnectionService.getBusConnectionByLineAndStop(busLineId, busStopId)) {
+            return new ResponseEntity(busConnectionService.getBusConnectionByLineAndStop(busLineId, busStopId), HttpStatus.OK);
+        } else {
+            return new ResponseEntity("Bus connection does not exist", HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
+    @GetMapping("/bus-connections/bus-lines/{busLineId}/bus-carriers/{carrierId}")
+    public @ResponseBody
+    ResponseEntity getBusLineByBusLineAndCarrier(@PathVariable("busLineId") String busLineId, @PathVariable("carrierId") String carrierId) {
+        if (null != busConnectionService.getBusConnectionByLineAndCarrier(busLineId, carrierId)) {
+            return new ResponseEntity(busConnectionService.getBusConnectionByLineAndCarrier(busLineId, carrierId), HttpStatus.OK);
+        } else {
+            return new ResponseEntity("Bus connection does not exist", HttpStatus.NOT_FOUND);
+        }
+
+    }
+}
